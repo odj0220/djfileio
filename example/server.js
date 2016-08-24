3a48ba7ffbc1495469a8a983d48443d9ab1bef89
odj0220 <odj0220@nate.com>
2016-08-24 12:21:33 +0900

    init
    init

---
+++ example/server.js
@@ -0,0 +1,82 @@
+var express = require('express'),
+    djFileIO = require('../index'),
+    formidable = require('formidable'),
+    fs = require('fs'),
+    app = express();
+
+djFileIO.config.host = '10.0.0.100';
+djFileIO.config.port = '27017';
+djFileIO.config.database = 'djcloud';
+djFileIO.config.type = 1;
+djFileIO.config.path = "D:/upload";
+
+
+/*djFileIO.getConnection(function(err, data){
+    console.log(err);
+});*/
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/view/index.html');
+});
+
+app.post('/upload', function(req, res){
+    console.log('업로드 서버 호출');
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+    form.on('fileBegin', function (name, file){
+
+    });
+
+    form.on('file', function (name, file){
+        djFileIO.fileUpload('admin','/',file, function(err, f){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/');
+            }
+        });
+    });
+});
+
+app.get('/list', function(req, res){
+    djFileIO.fileList(req.query.code, function(err, list){
+        djFileIO.properties(req.query.code, function(result){
+            res.jsonp({
+                list: list,
+                prop: result
+            });
+        });
+
+    });
+});
+
+app.get('/addFolder', function(req, res){
+    //function(userNo, parentCode, name, callback){
+    var parent = req.query.parent || '/';
+    var name = req.query.name;
+    djFileIO.addFolder('admin', parent, name, function(err, data){
+        res.jsonp(data);
+    })
+});
+
+app.get('/properties', function(req, res){
+    var code = req.query.code;
+    djFileIO.properties(code, function(result){
+        res.jsonp(result);
+    });
+});
+
+app.get('/delete', function(req, res){
+    var code = req.query.code;
+    djFileIO.fileDelete(code, function(result){
+        res.jsonp(result);
+    });
+});
+
+
+djFileIO.copyFile('7Te7j5wEVVp1EsgZseDl', null, function(err, success){
+    console.log(success);
+});
+
+app.listen(3000);
---
+++ index.js
@@ -0,0 +1,22 @@
+var con = require('./lib/Connection');
+var ctrl = require('./lib/Controller');
+var app = this;
+
+exports.config = {
+    type: null,     // db 종류
+    host: null,     // 연결 주소
+    port: null,     // 연결 포트
+    user: null,     // 아이디
+    password: null,     // 비밀번호
+    database: 'djcloud',
+    filePath: null  //파일 경로
+};
+con.config = app.config;
+
+exports.fileUpload = ctrl.fileUpload;
+exports.fileList = ctrl.fileList;
+exports.addFolder = ctrl.addFolder;
+exports.properties = ctrl.properties;
+exports.fileDelete = ctrl.fileDelete;
+exports.moveFile = ctrl.moveFile;
+exports.copyFile = ctrl.copyFile;
